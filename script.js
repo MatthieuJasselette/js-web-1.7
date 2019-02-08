669d6832df3568118a74b72e6ff2e111a16d9f66
matthieu <matthieu.jasselette@gmail.com>
2019-02-08 14:34:00 +0100

    final version with random comments

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let numberOfTry = 0;
 //returns a new array containing random picks from two other arrays
 let pickRandomPos = (arrA, arrB) => {
   let newPos = [];
-  let randomPercentX = Math.floor(Math.random()*666)+"px";
+  let randomPercentX = Math.floor(Math.random()*100)+"%";
   let randomPercentY = Math.floor(Math.random()*666)+"px";
   let randomX = Math.floor(Math.random()*Math.floor(arrA.length-1));
   newPos.push(arrA[randomX]+randomPercentX);
@@ -29,16 +29,25 @@ let pickRandomPos = (arrA, arrB) => {
   return   newPos;
 }
 
+let commentArray = ["Good job !", "Remember, you only fail when you give up.", "You're almost there.", "Don't give up, you can do it.", "We all believe you can do it.", "As long as you keep trying, you're bound to meet success."]
+
+let randomizeComment = (comArr) => {
+  let  randomComment = "";
+  randomComment = comArr[Math.floor(Math.random()*comArr.length)];
+  return randomComment;
+}
+
 //trigger on mouseover
 document.getElementById("trollButton").addEventListener('mouseover', () => {
   numberOfTry ++
   if(numberOfTry%5===0){
-    document.getElementById("trollButton").innerText=`You have tried ${numberOfTry} times.`;
-  } else {
-    document.getElementById("trollButton").innerText=`Click me !`;
+    window.alert(randomizeComment(commentArray) + ` You have tried ${numberOfTry} times.`);
+  //   document.getElementById("trollButton").innerText=`You have tried ${numberOfTry} times.`;
+  // } else {
+  //   document.getElementById("trollButton").innerText=`Click me !`;
   }
   let newPos = pickRandomPos(xAxisArr, yAxisArr); //contains an array
-  document.getElementById("buttonBox").style = 'position:absolute ;' + newPos[0] + '; ' + newPos[1] +';';
+  document.getElementById("trollButton").style = 'position:absolute ;' + newPos[0] + '; ' + newPos[1] +';';
 
 ;
   console.log(newPos);
